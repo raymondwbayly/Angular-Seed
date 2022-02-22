1e680937dbc4c4d0855106da736144e7951ba550
Raymond Bayly <63461512+raymondwbayly@users.noreply.github.com>
2022-02-21 21:09:49 -0500

    update code

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { SupportComponent } from './support/support.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompanyComponent,
+    SupportComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
